cda5dd97d8c6bc9ac2fd9c9475c1e5b584ed6dd4
Kevin Britz <kevin@equilibria.xyz>
2022-01-11 00:01:03 -0800

    upgrade to 0.8.11

--- contracts/batcher/Batcher.sol
+++ contracts/batcher/Batcher.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.10;
+pragma solidity 0.8.11;
 
 import "@equilibria/root/types/UFixed18.sol";
 import "@equilibria/root/types/Token18.sol";
