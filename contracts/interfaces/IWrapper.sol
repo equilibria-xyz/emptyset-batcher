1e311439f0949eafc09526f92aa3101ea382e09f
Fennel <0xfennel@proton.me>
2023-10-19 11:12:33 -0700

    Add USDC/DSU to wrapper interface

--- contracts/interfaces/IWrapper.sol
+++ contracts/interfaces/IWrapper.sol
@@ -1,7 +1,13 @@
 //SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.17;
 
+import "@equilibria/root/token/types/Token6.sol";
+import "@equilibria/root/token/types/Token18.sol";
+
 interface IWrapper {
+    function USDC() external view returns (Token6); // solhint-disable-line func-name-mixedcase
+    function DSU() external view returns (Token18); // solhint-disable-line func-name-mixedcase
+
     function wrap(address to) external;
     function unwrap(address to) external;
 }
